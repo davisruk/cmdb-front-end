4a38448f5cfec9b65cfb1dda7554384b85a2d8e6
Richard Davis <rich@davisfamily.eu>
2017-02-11 10:22:59 +0000

    Fixed BIIIIG user delete bug

--- app/entities/user/user-detail.component.ts
+++ app/entities/user/user-detail.component.ts
@@ -39,6 +39,7 @@ export class UserDetailComponent implements OnInit, OnDestroy {
 
             if (id === 'new') {
                 this.user = new User();
+                this.user.enabled = false;
                 this.roleService.getAllRoles().subscribe(p=>this.availableRoles = p);
                 this.user.roles = new Array<Role>();
             } else {
